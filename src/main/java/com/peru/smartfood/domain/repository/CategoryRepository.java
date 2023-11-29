7569e8700f86c2a5cff96048ed82ad39c3709e65
Hans Retes <hans_2801_7@hotmail.com>
2023-11-29 12:52:16 -0500

    heroku update

--- src/main/java/com/peru/smartfood/domain/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.peru.smartfood.domain.repository;
-
-import com.peru.smartfood.domain.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
--- src/main/java/com/peru/smartfood/domain/repository/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.peru.smartfood.domain.repository;
-
-import com.peru.smartfood.domain.model.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
-}
